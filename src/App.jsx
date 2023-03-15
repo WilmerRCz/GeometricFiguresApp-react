0e306e03d540a1cfc0fef26baeec85539b838cf7
Wilmer Rodriguez <wilmerr09@gmail.com>
2023-03-15 14:41:27 -0300

    Creando Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='font-bold text-white text-2xl m-4'>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
